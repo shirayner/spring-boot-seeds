eac2c0f201f5ea3e288b9e7681be573d22dc0ea5
shirayner <1766211120@qq.com>
2019-05-07 14:29:20 +0800

    [ADD] SpringBoot_03_模板引擎_01_整合Thymeleaf

---
+++ spring-boot-03-thymeleaf/src/main/java/com/ray/study/springboot03thymeleaf/SpringBoot03ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.ray.study.springboot03thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot03ThymeleafApplication.class, args);
+	}
+
+}
---
+++ spring-boot-03-thymeleaf/src/main/java/com/ray/study/springboot03thymeleaf/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.ray.study.springboot03thymeleaf.controller;
+
+
+import com.ray.study.springboot03thymeleaf.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * UserController
+ *
+ * @author shira 2019/04/28 15:45
+ */
+@Controller
+@RequestMapping(value = "/users")     // 通过这里配置使下面的映射都在/users下
+public class UserController {
+
+
+	@GetMapping("/index1")
+	public ModelAndView index1() {
+
+		List<User> userList = listUser();
+
+		ModelAndView view = new ModelAndView();
+		// 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
+		view.setViewName("index");
+
+		// 设置属性
+		view.addObject("title", "SpringBoot 整合 Thymeleaf");
+		view.addObject("userList", userList);
+
+		return view;
+	}
+
+
+	@GetMapping("/index2")
+	public String index2(Model model) {
+		List<User> userList = listUser();
+
+		model.addAttribute("title", "SpringBoot 整合 Thymeleaf");
+		model.addAttribute("userList", userList);
+
+		// 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
+		return "index";
+	}
+
+
+
+	private List<User> listUser(){
+		List<User> userList = new ArrayList<>();
+		for (int i=0 ; i<3; i++){
+			userList.add(new User(1L+i,"tom"+i,21+i));
+		}
+
+		return userList;
+	}
+
+}
