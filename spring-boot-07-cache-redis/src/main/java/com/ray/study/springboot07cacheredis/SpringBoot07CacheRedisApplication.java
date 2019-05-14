003346a9d7da880924fe6a97dd874ee1025c6057
shirayner <1766211120@qq.com>
2019-05-14 16:31:53 +0800

    [ADD] SpringBoot_07_Cache_04_Redis

---
+++ spring-boot-07-cache-redis/src/main/java/com/ray/study/springboot07cacheredis/SpringBoot07CacheRedisApplication.java
@@ -0,0 +1,13 @@
+package com.ray.study.springboot07cacheredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07CacheRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot07CacheRedisApplication.class, args);
+	}
+
+}
